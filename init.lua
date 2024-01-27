f60c1841f20bbf9e523723f53e78eafbf10dca31
Gunawan Nur Ahmad <gunawan.ahmad@incentro.com>
2024-01-27 19:34:50 +0800

    add lsp and change theme

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader= " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
- 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
